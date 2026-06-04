fea930c031eb9de0fc4527be66ca60b31869d61e
Anthony <afrodriguez@sudamericano.edu.ec>
2026-06-04 09:37:28 -0500

    Implementar TareasController con rutas CRUD
    Agrega los endpoints GET, POST, PATCH y DELETE para gestionar las tareas desde el frontend

--- src/tareas/tareas.controller.ts
+++ src/tareas/tareas.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { TareasService } from './tareas.service'
 
 @Controller('tareas')
-export class TareasController {}
+export class TareasController {
+
+    constructor(private readonly tareasService: TareasService) {}
+
+    @Get()
+    obtenerTodas() {
+        return this.tareasService.obtenerTodas();
+    }
+
+    @Post()
+    crear(@Body('titulo') titulo: string) {
+        return this.tareasService.crear(titulo);
+    }
+
+    @Patch(':id')
+    actualizar(@Param('id', ParseIntPipe) id: number, @Body('hecha') hecha: boolean) {
+        return this.tareasService.actualizar(id, hecha);
+    }
+
+    @Delete(':id')
+    eliminar(@Param('id', ParseIntPipe) id: number) {
+        return this.tareasService.eliminar(id);
+    }
+
+}
