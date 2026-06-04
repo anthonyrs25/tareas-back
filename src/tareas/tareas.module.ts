a45042afab84678b27af65b33af8757851cdc617
Anthony <afrodriguez@sudamericano.edu.ec>
2026-06-04 09:13:14 -0500

    Registrar PrismaService en Tareas Module
    Agregar PrismaService como provider para que TareasService pueda acceder a la base de datos.

--- src/tareas/tareas.module.ts
+++ src/tareas/tareas.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TareasService } from './tareas.service';
 import { TareasController } from './tareas.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [TareasService],
+  providers: [TareasService, PrismaService],
   controllers: [TareasController]
 })
 export class TareasModule {}
